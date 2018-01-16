63b4036f8d7802a0d25651fad8af5deedb4f10b7
Lasha Efremidze <efremidzel@hotmail.com>
2018-01-16 01:46:00 -0800

    Update Gyro.swift

--- Sources/Gyro.swift
+++ Sources/Gyro.swift
@@ -14,28 +14,26 @@ public let Gyro = GyroManager.shared
 open class GyroManager: CMMotionManager {
     open static let shared = GyroManager()
     private let queue = OperationQueue()
-    open func observe(_ observer: @escaping (CGFloat, CGFloat, CGFloat) -> Void) {
+    open func observe(_ observer: @escaping (_ roll: Double, _ pitch: Double, _ yaw: Double) -> Void) {
         guard isDeviceMotionAvailable else { return }
-        deviceMotionUpdateInterval = 0.1
+        deviceMotionUpdateInterval = 1 / 60
         startDeviceMotionUpdates(to: queue) { data, error in
             guard let data = data else { return }
-            let roll = CGFloat(data.attitude.roll.degrees)
-            var pitch = CGFloat(data.attitude.pitch.degrees)
-            let yaw = CGFloat(data.attitude.yaw.degrees)
+            var pitch = data.attitude.pitch
             if data.gravity.z > 0 {
                 if pitch > 0 {
-                    pitch = 180 - pitch
+                    pitch = .pi - pitch
                 } else {
-                    pitch = -(180 + pitch)
+                    pitch = -(.pi + pitch)
                 }
             }
             DispatchQueue.main.sync {
-                observer(roll, pitch, yaw)
+                observer(data.attitude.roll, pitch, data.attitude.yaw)
             }
         }
     }
 }
 
-extension FloatingPoint {
+public extension FloatingPoint {
     var degrees: Self { return self * 180 / .pi }
 }
